7fa75483878c8e08a17e1f3632ac1a2d3a833266
amhayslip <amhayslip@gmail.com>
2013-11-22 11:44:55 -0600

    finished friday eval

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 
 // Using NaN instead of null is a clever hack. See checkForWinner for details.
+
 var spaces = [
   NaN, NaN, NaN,
   NaN, NaN, NaN,
   NaN, NaN, NaN
 ];
 
+veggiewins = []
+junkfoodwins = []
+
 var player1 = 'veggies';
 var player2 = 'junkfood';
 var currentPlayer = null;
+var winner = null;
 
 var setNextTurn = function () {
   if (currentPlayer === player1) {
@@ -28,31 +33,51 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
     // TODO: Check for rest of game winning cases
   )
   {
-    console.log('somebody won');
+    winner = currentPlayer
+    
+
+  $(document).on('game-win', function (e, winner) {
+      console.log(winner + ' has won');
+  });
+
+  $(document).trigger('game-win', winner );
     // TODO: Trigger 'game-win' event with the winning player as the event data
   }
 };
 
-$(document).on('click', '#board .space', function (e) {
-  var spaceNum = $(e.currentTarget).index();
-  console.log('You clicked on space #' + spaceNum);
 
+$(document).on('click', '#board .space', function (e) {
+var spaceNum = $(e.currentTarget).index();
+  if (spaces[spaceNum]) {
+     alert("You can't do that!");
+     console.log(spaces[spaceNum]);
+  }
+  else {
+     console.log(currentPlayer + ' clicked on space #' + spaceNum);
   // Mark the space with the current player's name
   // TODO: Don't mark it unless the space is blank
-  spaces[spaceNum] = currentPlayer;
+     spaces[spaceNum] = currentPlayer;
   // Add class to elem so css can take care of the visuals
-  $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
-
+     $('#board .space:eq(' + spaceNum + ')').addClass(currentPlayer);
   checkForWinner();
   setNextTurn();
+  }
 });
 
 $(document).on('game-win', function (e, winner) {
-  // TODO: Alert who won the game
+    alert(winner + " has won!");
+  // TODO: Alert who won the game CHECK
 });
+
+
 
 // Start the game
 setNextTurn();
